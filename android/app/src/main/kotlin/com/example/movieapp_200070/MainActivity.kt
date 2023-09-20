10702b57e1c436ff888ef3dca36ebf848c7c39c0
MauricioRL15 <15rlmauricio@gmail.com>
2023-09-20 11:45:30 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/movieapp_200070/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.movieapp_200070
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:movieapp_200070/home.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home : Home(),
+    theme: new ThemeData.dark(),
+  ));
+
+
+
+}
